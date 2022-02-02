f677742c5330dbf96cdc44355b4c17b4488bc83c
François <mockersf@gmail.com>
2022-02-02 02:10:59 +0100

    setup ci

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+// disable console opening on windows
+#![windows_subsystem = "windows"]
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("icon.png"),
+        ..Default::default()
+    });
+}
