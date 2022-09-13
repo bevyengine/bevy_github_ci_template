3a6bd86d861dbd782f3e34698a4a73cee7355729
Rob Parrett <robparrett@gmail.com>
2022-09-13 11:51:59 -0700

    Allow some clippy lints that complain about common bevy code (#16)
    * Allow some clippy lints that complain about common bevy code
    
    * Don't ignore the lints in CI
    
    * Add a comment explaining the allowing of these lints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// Bevy code commonly triggers these lints and they may be important signals
+// about code quality. They are sometimes hard to avoid though, and the CI
+// workflow treats them as errors, so this allows them throughout the project.
+// Feel free to delete this line.
+#![allow(clippy::too_many_arguments, clippy::type_complexity)]
+
 use bevy::prelude::*;
 
 fn main() {
