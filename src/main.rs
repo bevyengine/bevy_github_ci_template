8caf59975cb566567da14d52935ea66c80d4eada
PraxTube <lukinator28@gmail.com>
2023-12-13 19:54:40 +0100

    chore: Bump to Bevy 0.12

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        // Wasm builds will check for meta files (that don't exist) if this isn't set.
+        // This causes errors and even panics on web build on itch.
+        // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
         .insert_resource(AssetMetaCheck::Never)
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
