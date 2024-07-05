69aad795c20345aa3a040a3b0e9413430302ada0
Rob Parrett <robparrett@gmail.com>
2024-07-05 06:26:24 -0700

    Update to Bevy 0.14

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,13 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        // Wasm builds will check for meta files (that don't exist) if this isn't set.
-        // This causes errors and even panics on web build on itch.
-        // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
-        .insert_resource(AssetMetaCheck::Never)
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(AssetPlugin {
+            // Wasm builds will check for meta files (that don't exist) if this isn't set.
+            // This causes errors and even panics in web builds on itch.
+            // See https://github.com/bevyengine/bevy_github_ci_template/issues/48.
+            meta_check: AssetMetaCheck::Never,
+            ..default()
+        }))
         .add_systems(Startup, setup)
         .run();
 }
